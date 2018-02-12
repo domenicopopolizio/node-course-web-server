a6a13383ed8963920208ef4b5a4184b22bdabc8a
domenicopopolizio <domenico9501@outlook.it>
2018-02-12 11:05:16 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+
+app.use( (req, res, next) => res.render('maintenance.hbs') );
+
+app.use(express.static(__dirname+'/public'));
+
+app.use(
+    (req, res, next) => {
+        let now = new Date().toString();
+        let log = `${now}: ${req.method} ${req.url}`;
+        fs.appendFile('server.log', log+'\n', 
+            err => console.log('Can\'t Save file, because following error occurred: \n', err)
+        );
+        next();
+    }
+)
+
+
+
+hbs.registerHelper(
+    'getCurrentYear',
+    () => new Date().getFullYear()
+)
+
+hbs.registerHelper(
+    'screamIt',
+    text => text.toUpperCase()
+)
+
+
+app.get(
+    '/',
+    (req, res) => {
+        //res.send('<h1>Hello Express!</h1>');
+        res.render(
+            'index.hbs',
+            {
+                pageTitle: 'Home Page',
+                welcomeMessage: 'Hey Welcome to my brand new site!',
+                currentYear: new Date().getFullYear()
+            }
+        )
+    }
+)
+
+
+app.get(
+    '/about',
+    (req, res) => {
+        res.render(
+            'about.hbs',
+            {
+                pageTitle: 'About Page',
+                currentYear: new Date().getFullYear()
+            }
+        );
+    }
+)
+
+app.get(
+    '/bad',
+    (req, res) => {
+        res.send(
+            {
+                errorMessage: 'Unable to handle request!'
+            }
+        );
+    }
+)
+
+app.listen(
+    3000,
+    () =>{
+        console.log('Server up is up in port 3000');
+    }
+);
